7b171e3d4f8a8d58a0fe0418852f0805a9bfb474
alistairing <alistairing91@gmail.com>
2022-01-25 15:16:32 +0000

    script to download recipes into dataframe

---
+++ process-recipes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import json
+
+# set the target url
+target_url = "http://openrecipes.s3.amazonaws.com/openrecipes.txt"
+
+# get the JSON style content
+response = requests.get(target_url)
+content = response.text
+
+# clean the content so that it can be converted by json.loads() function
+# 1) enclose with square brackets
+# 2) insert commas after each } 
+# 3) remove final comma
+data = '['+ content + ']'
+data = data.replace("}", "},")
+data = data[:-3]+data[-2:]
+
+# convert from string of json format into python object 
+data_json = json.loads(data)
+
+# convert into dataframe
+df = pd.DataFrame(data_json)
+print(df.head())
+
+
